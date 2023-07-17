29f063c9fdffc140e780b3a9f23752b26ac20b9b
mmiranda55 <139494573+mmiranda55@users.noreply.github.com>
2023-07-17 15:59:53 -0300

    SearchViewModel: Get from URL the results and active navigation to ProductListView

--- MyChallenge.swiftpm/ChallengeMELI/Views/SearchView/SearchViewModel.swift
+++ MyChallenge.swiftpm/ChallengeMELI/Views/SearchView/SearchViewModel.swift
@@ -4,6 +4,7 @@ import Foundation
 class SearchViewModel: ObservableObject {
     @Published var searchText = ""
     @Published var products: [Product] = []
+    @Published var isShowingProductList = false
     
     private var cancellables = Set<AnyCancellable>()
     
@@ -17,5 +18,25 @@ class SearchViewModel: ObservableObject {
             return
         }
         
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: SearchResult.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error en la solicitud: \(error.localizedDescription)")
+                }
+            } receiveValue: { result in
+                self.products = result.results
+                if result.results.isEmpty {
+                    print("No se encontraron resultados")
+                } else {
+                    self.isShowingProductList = true
+                }
+            }
+            .store(in: &cancellables)
     }
 }
