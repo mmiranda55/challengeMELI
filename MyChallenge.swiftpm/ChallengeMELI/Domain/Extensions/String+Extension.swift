817cf03dd4fd6fc0dbefd68b707ddcd62b20231c
mmiranda55 <139494573+mmiranda55@users.noreply.github.com>
2023-07-18 03:01:44 -0300

    Extensions: String and Text()

---
+++ MyChallenge.swiftpm/ChallengeMELI/Domain/Extensions/String+Extension.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+extension String {
+    static let titleSearch = "¿Buscando algo en particular?"
+    static let freeShipping = "Envío gratis"
+    static let errorTitle = "Lo sentimos, ha ocurrido un error"
+    static let errorMessage = "No pudimos encontrar artículos relacionados a tu búsqueda"
+}
---
+++ MyChallenge.swiftpm/ChallengeMELI/Domain/Extensions/Text+Extension.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+extension Text {
+    init(_ string: String) {
+        self.init(verbatim: string)
+    }
+}
