3af91f494a5bd01330cdf6f885a0fd351f2c175b
mmiranda55 <139494573+mmiranda55@users.noreply.github.com>
2023-07-18 23:36:15 -0300

    Product: Add new variables

--- MyChallenge.swiftpm/ChallengeMELI/Domain/Models/Product.swift
+++ MyChallenge.swiftpm/ChallengeMELI/Domain/Models/Product.swift
@@ -4,18 +4,20 @@ struct Product: Codable, Identifiable {
     let id: String
     let title: String
     let price: Double
+    let mercadoPagoAvailable: Bool
     let shipping: Shipping
-    let availableQuantity: Double
-    let soldQuantity: Double
-    let condition: String
+    let availableQuantity: Int
+    let soldQuantity: Int
+    let thumbnail: String
     let attributes: [ProductDetail]
     
     enum CodingKeys: String, CodingKey {
         case id
-        case condition
         case title
         case price
         case shipping
+        case mercadoPagoAvailable = "accepts_mercadopago"
+        case thumbnail
         case availableQuantity = "available_quantity"
         case soldQuantity = "sold_quantity"
         case attributes
@@ -31,12 +33,15 @@ extension Product {
         }
     }
 }
+
 extension Product {
-    struct ProductDetail: Codable {
-        let name: String? = ""
-        let value: String? = ""
+    struct ProductDetail: Codable, Identifiable {
+        let id: String
+        let name: String = ""
+        let value: String = ""
         
         enum CodingKeys: String, CodingKey {
+            case id
             case name
             case value = "value_name"
         }
